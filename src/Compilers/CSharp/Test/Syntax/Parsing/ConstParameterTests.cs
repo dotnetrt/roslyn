1b17b278d71c2a380700e4f8b5698077b23cbfb1
Jacek Blaszczynski <biosciencenow@outlook.com>
2017-09-12 18:15:30 +0200

    Add more parser tests

--- src/Compilers/CSharp/Test/Syntax/Parsing/ConstParameterTests.cs
+++ src/Compilers/CSharp/Test/Syntax/Parsing/ConstParameterTests.cs
@@ -22,64 +22,209 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
         }
 
         [Fact]
-        public void TestConstParameter001()
+        public void MethodReturnsVoidWithConstPar()
         {
-            var text = "public class A { public void TestConst(const int value){ return; } }";
-            var file = this.ParseFile(text);
+            var text =
+@"public class A
+{
+    public void TestConst(const int value)
+    {
+        return;
+    }
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
 
             VerifyTestPassed(text, file);
         }
 
         [Fact]
-        public void TestConstParameter002()
+        public void MethodReturnsVoidWithRefParConstPar()
         {
-            var text = "public class A { public void TestConst(ref int input, const int value){ return; } }";
-            var file = this.ParseFile(text);
+            var text =
+@"public class A
+{
+    public void TestConst(ref int input, const int value)
+    {
+        return;
+    }
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
 
             VerifyTestPassed(text, file);
         }
 
         [Fact]
-        public void TestConstParameter003()
+        public void GenericMethodReturnsVoidWithRefParConstPar()
         {
-            var text = "public class A { public void TestConst<T, U>(ref int input, const int value){ return; } }";
-            var file = this.ParseFile(text);
+            var text =
+@"public class A
+{
+    public void TestConst<T, U>(ref int input, const int value)
+    {
+        return;
+    }
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
 
             VerifyTestPassed(text, file);
         }
 
         [Fact]
-        public void TestConstParameter004()
+        public void GenericMethodReturnsVoidWithRefGenericParConstParAndGenericParams()
         {
-            var text = "public class A { public void TestConst<T, U>(ref T input, const int value, params U[] list){ return; } }";
-            var file = this.ParseFile(text);
+            var text =
+@"public class A
+{
+    public void TestConst<T, U>(ref T input, const int value, params U[] list)
+    {
+        return;
+    }
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
 
             VerifyTestPassed(text, file);
         }
 
         [Fact]
-        public void TestConstParameter005()
+        public void GenericMethodReturnsVoidWithConstParAndGenericOutPar()
         {
-            var text = "public class A { public void TestConst<T>(const int input, out T value){ value = null; return; } }";
-            var file = this.ParseFile(text);
+            var text =
+@"public class A
+{
+    public void TestConst<T>(const int input, out T value)
+    {
+        value = null;
+        return;
+    }
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
 
             VerifyTestPassed(text, file);
         }
 
         [Fact]
-        public void TestConstParameter006()
+        public void AsyncGenericMethodReturnsTaskWithConstParAndGenericOutPar()
         {
-            var text = "public class A { public async Task TestConstAsync<T>(const int input, out T value){ value = null; return; } }";
-            var file = this.ParseFile(text);
+            var text =
+@"public class A
+{
+    public async Task TestConstAsync<T>(const int input, out T value)
+    {
+        value = null;
+        return;
+    }
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
 
             VerifyTestPassed(text, file);
         }
 
         [Fact]
-        public void TestConstParameter007()
+        public void AsyncGenericMethodReturnsTaskUWithConstParAndGenericOutPar()
         {
-            var text = "public class A { public async Task<U> TestConstAsync<T, U>(const int input, out T value) where U : struct { value = null; return default(U); } }";
-            var file = this.ParseFile(text);
+            var text =
+@"public class A
+{
+    public async Task<U> TestConstAsync<T, U>(const int input, out T value) where U : struct
+    {
+        value = null;
+        return default(U);
+    }
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
+
+            VerifyTestPassed(text, file);
+        }
+
+        [Fact]
+        public void DelegateWithRefParConstPar()
+        {
+            var text =
+@"public class A
+{
+    public delegate void DelegateWithConst(ref int input, const int value);
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
+
+            VerifyTestPassed(text, file);
+        }
+
+        [Fact]
+        public void ConstructorWithRefParConstPar()
+        {
+            var text =
+@"public class A
+{
+    public A(ref int input, const int value)
+    {
+    }
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
+
+            VerifyTestPassed(text, file);
+        }
+
+        [Fact]
+        public void AnonymousMethodWithConstParConstPar()
+        {
+            var text =
+@"public class A
+{
+    public delegate void DelegateWithConst(const int errorCode, const string errorMessage);
+    public event DelegateWithConst PrintError = delegate (const int errorId, const string messageTxt) { WriteMessage(errorId, messageTxt); };
+    private void WriteMessage(int value, string message) {}
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
+
+            VerifyTestPassed(text, file);
+        }
+
+        [Fact]
+        public void LambdaWithExplicitlyTypedConstParConstPar()
+        {
+            var text =
+@"public class A
+{
+    public delegate void DelegateWithConst(const int errorCode, const string errorMessage);
+    public event DelegateWithConst PrintError = (const int errorId, const string messageTxt) => { WriteMessage(errorId, messageTxt); };
+    private void WriteMessage(int value, string message) {}
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
+
+            VerifyTestPassed(text, file);
+        }
+
+        [Fact]
+        public void ExplicitConversionOperatorWithParConstPar()
+        {
+            var text =
+@"public class A
+{
+}
+
+public class B
+{
+    public B(A value, const int shift)
+    {
+    }
+    public explicit operator B(A value, const byte shift)
+    {
+        return new B(A, const shift);
+    }
+}
+";
+            var file = this.ParseFileExperimental(text, MessageID.IDS_FeatureConstParameters);
 
             VerifyTestPassed(text, file);
         }
@@ -88,8 +233,9 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
         {
             Assert.NotNull(file);
             Assert.Equal(text, file.ToString());
-            Assert.False(file.HasErrors);
-            Assert.False(file.Warnings().Length > 0);
+            var error = file.Errors().FirstOrDefault();
+            Assert.False(file.HasErrors, $"Compiler Error {error?.MessageIdentifier}: {error?.MessageProvider.LoadMessage((int)error?.Code, System.Globalization.CultureInfo.InstalledUICulture)}");
+            Assert.False(file.Warnings().Length > 0, $"Compiler Warning {file.Warnings().FirstOrDefault()?.MessageIdentifier}");
         }
     }
 }
